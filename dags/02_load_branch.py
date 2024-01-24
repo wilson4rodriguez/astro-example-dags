a2f4d2f21cae7ac3a0820a583f7bf629c414abbf
Wilson Camilo Rodriguez <147677174+wilson4rodriguez@users.noreply.github.com>
2024-01-23 21:14:58 -0500

    Update 02_load_branch.py
    All done

--- dags/02_load_branch.py
+++ dags/02_load_branch.py
@@ -71,6 +71,7 @@ with DAG(
     load_bi = PythonOperator(
         task_id='load_bi',
         python_callable=fun_load_bi,
+        trigger_rule=TriggerRule.ALL_DONE,
         dag=dag
     )
     load_raw>>branch>>[load_master_complete,load_master_delta]>>load_bi
